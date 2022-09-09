88a3dfc847e47f71bac0bffc68cfd8c61d8407c5
Melih Uraz <melihuraz@hotmail.com>
2022-09-09 14:56:16 +0200

    Change code

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
+import * as fs from 'node:fs';
 import * as cheerio from 'cheerio';
 import fetch from 'node-fetch';
 
-//jquery.
-// const url = "https://memegen-link-examples-upleveled.netlify.app";
+const dir = './memes';
+fs.mkdir(dir, (err) => {
+  if (dir) return;
+  if (err) {
+    throw err;
+  }
+  console.log('directory created');
+});
 
 const response = await fetch(
-  'https://memegen-link-examples-upleveled.netlify.app',
+  'https://memegen-link-examples-upleveled.netlify.app/',
 );
-const img = await response.text();
+const body = await response.text();
+const $ = cheerio.load(body);
 
-const image = cheerio.load(img);
+let div = $('section > div');
+// recieve all parents of images
+let srcs = [];
+div.each(function () {
+  // add this value to the array
+  srcs.push($(this).find('img').attr('src'));
+});
 
-// console.log(image);
-// const urlArray = [];
-const src = image('img').attr('src');
-console.log(src);
+const slice = srcs.slice(0, 10);
+
+let buffers = [];
+
+for (let i = 0; i < slice.length; i++) {
+  const imageData = await fetch(slice[i]);
+
+  const arrayBuffer = await imageData.arrayBuffer();
+
+  const buffer = Buffer.from(arrayBuffer);
+  fs.writeFile(`./memes/0${i + 1}.jpg`, buffer, function (err) {});
+}
